0547f20d81f4a731f4e4a9c61d351affb2d6a71c
kjartanh <kheimission@gmail.com>
2023-05-10 15:45:13 +0100

    first commit, complete react app

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+
+import { useEffect, useState } from "react"
+import { NewTodoForm } from "./NewTodoForm"
+import "./styles.css"
+import { TodoList } from "./TodoList"
+
+export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+    
+    return JSON.parse(localValue) // all this is to store the data on the site, doesnt delete after refresh
+  }) 
+  // checking localStorage.. getting the value IF it exists
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos]) 
+  // every time we modify the todos, we run the function and save the value of todos to localStorage
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos, 
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  console.log(todos)
+
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
+  )
+}
---
+++ src/TodoList.jsx
@@ -0,0 +1,13 @@
+import { TodoItem } from "./TodoItem"
+
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
+    return (
+      <ul className="List">
+        {todos.length === 0 && "No todos"}
+        {/* unique identifier (todos.id) key={todos.id} */}
+        {todos.map(todo => {
+          return <TodoItem {...todo} key={todo.id} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+        })}
+      </ul>
+    )
+}
